35577fac2946c5523900916e0d8a63077441d87c
EC2 Default User <ec2-user@ip-172-31-20-1.ap-northeast-1.compute.internal>
2022-07-16 18:17:05 +0000

    [Add] 削除機能

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     get '/' => 'homes#top'
-    get 'books/new' => 'books#new'
-    post 'books' => 'books#create'
-    get 'books' => 'books#index'
-    get 'books/:id' => 'books#show', as:'book'
-    get 'books/:id/edit' => 'books#edit', as:'edit_book'
-    patch 'books/:id' => 'books#update', as:'update_book'
     resources :books
 end
